fb8a6be9b731ab64a2b6ed582a31056fc557b7bf
Akash Warkhad <55613414+AkashWarkhad@users.noreply.github.com>
2025-02-17 10:40:38 +0530

    Feature/nz 1004 small changes (#5)
    * Added logger into controller
    
    * adjustments
    
    * adjustments3

---
+++ Web-API-Versioning.API/Web-API-Versioning.API/Models/DTO/CountryDtoV2.cs
@@ -0,0 +1,9 @@
+﻿namespace Web_API_Versioning.API.Models.DTO
+{
+    public class CountryDtoV2
+    {
+        public int ID { get; set; }
+
+        public string CountryName { get; set; }
+    }
+}
